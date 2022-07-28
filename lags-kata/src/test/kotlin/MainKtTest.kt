152dea762809ffe9e6355f466e824cab20aa7cb4
Jonathan Nupnau <jonathan.nupnau@colenet.de>
2022-07-28 17:13:23 +0200

    empty todo

--- lags-kata/src/test/kotlin/MainKtTest.kt
+++ lags-kata/src/test/kotlin/MainKtTest.kt
@@ -2,6 +2,12 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class MainKtTest {
+
+    /*
+     * TODO:
+     *  - Write first failing test
+     */
+
     @Test
     internal fun test_me() {
         assertThat(1).isEqualTo(1)
