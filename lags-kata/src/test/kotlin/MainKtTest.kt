0068d3e39ed6722be92be6aa3857c6dd8c8e9036
Jonathan Nupnau <jonathan.nupnau@colenet.de>
2022-07-28 15:08:12 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- lags-kata/src/test/kotlin/MainKtTest.kt
+++ lags-kata/src/test/kotlin/MainKtTest.kt
@@ -1,9 +1,28 @@
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
+data class Flight( val id: String, val start: Int, val duration: Int, val price: Int )
+
+private fun calculatePrice(combination: List<Flight>): Int {
+    return combination.sumOf { it.price }
+}
+
 internal class MainKtTest {
+
+    /* Possible request input
+        AF514 0 5 10
+        CO5 3 7 14
+        AF515 5 9 7
+        BA01 6 9 8
+    */
+
     @Test
-    internal fun test_me() {
-        assertThat(1).isEqualTo(1)
+    internal fun should_return_total_price_of_given_combination() {
+        val combination = listOf(
+            Flight("AF514", 0,5,10),
+            Flight("BA01", 6,9,8)
+        )
+        assertThat(calculatePrice(combination)).isEqualTo(18);
     }
+
 }
