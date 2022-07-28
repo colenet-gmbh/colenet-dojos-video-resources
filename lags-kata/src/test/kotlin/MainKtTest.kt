5083e4ab0047d3327546f088d921464d4a5d7be7
Johannes Jasper <johannes.jasper@colenet.de>
2022-07-28 15:58:33 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:lags-kata/src/test/kotlin/MainKtTest.kt

--- lags-kata/src/test/kotlin/MainKtTest.kt
+++ lags-kata/src/test/kotlin/MainKtTest.kt
@@ -14,8 +14,7 @@ internal class MainKtTest {
     @Test
     internal fun should_return_total_price_of_given_combination() {
         val combination = listOf(
-                Flight("AF514", 0,5,10),
-                Flight("BA01", 6,9,8)
+            Flight("AF514", 0, 5, 10), Flight("BA01", 6, 9, 8)
         )
         assertThat(calculatePrice(combination)).isEqualTo(18);
     }
@@ -28,37 +27,58 @@ internal class MainKtTest {
 
     @Test
     internal fun should_return_price_of_single_entry_for_one_element_list() {
-        val combination = listOf(Flight("B", 6,9,643))
+        val combination = listOf(Flight("B", 6, 9, 643))
         assertThat(calculatePrice(combination)).isEqualTo(643)
     }
 
     @Test
     internal fun findFollowUpFlights_should_return_list_of_follow_up_flights() {
-        val startFlight = Flight("B", 0,6,643)
+        val startFlight = Flight("B", 0, 6, 643)
         val possibleFlights = listOf(
-                Flight("AF514", 0,5,10),
-                Flight("BA01", 6,9,8)
+            Flight("AF514", 0, 5, 10), Flight("BA01", 6, 9, 8)
         )
         val followUpFlights = findFollowUpFlights(startFlight, possibleFlights)
 
-        val expected = listOf(Flight("BA01", 6,9,8))
+        val expected = listOf(Flight("BA01", 6, 9, 8))
         assertThat(followUpFlights).containsExactlyElementsOf(expected);
     }
 
     @Test
     internal fun findFollowUpFlights_should_return_list_of_two_follow_up_flights() {
-        val startFlight = Flight("B", 0,6,643)
+        val startFlight = Flight("B", 0, 6, 643)
         val possibleFlights = listOf(
-            Flight("AF514", 0,5,10),
-            Flight("BA01", 6,9,8),
-            Flight("BA05", 15,1,1),
+            Flight("AF514", 0, 5, 10),
+            Flight("BA01", 6, 9, 8),
+            Flight("BA05", 15, 1, 1),
         )
         val followUpFlights = findFollowUpFlights(startFlight, possibleFlights)
 
-        val expected = listOf(Flight("BA01", 6,9,8),  Flight("BA05", 15,1,1))
+        val expected = listOf(
+            Flight("BA01", 6, 9, 8), Flight("BA05", 15, 1, 1)
+        )
         assertThat(followUpFlights).containsExactlyElementsOf(expected);
+        assertThat(followUpFlights).doesNotContain(Flight("AF514", 0, 5, 10));
     }
 
+    @Test
+    internal fun `find valid combinations of flights`() {
+        val startFlight = Flight("B", 0, 6, 643)
+        val possibleFlights = listOf(
+            Flight("AF514", 0, 5, 10),
+            Flight("BA01", 6, 9, 8),
+        )
+
+        val expected = listOf(
+            listOf(startFlight),
+            listOf(
+                Flight("B", 0, 6, 643),
+                Flight("BA01", 6, 9, 8)
+            )
+        )
+        val result:List<List<Flight>> = findCombinations(startFlight, possibleFlights)
+        assertThat(result).isEqualTo(expected)
+
+    }
 
 
 }
